aa9b6050847f4b710d3da410df406ac389fd3467
anto__dev <antonidwm@gmail.com>
2023-07-24 00:45:39 +0100

    ADD current hrs function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,12 +9,13 @@ console.table(data);
 /// ADD data to the page
 
 let allPreviousHr = document.querySelectorAll('.previousHr');
+let allCurrentHr = document.querySelectorAll('.currentHr');
 
 /// previousHrs data 
-let i = 0;
+
 
 function dataChange(dateTime){
-    
+    let i = 0;
     allPreviousHr.forEach((el)=>{
 
         let allDataEl = data[i].timeframes;
@@ -32,6 +33,23 @@ function dataChange(dateTime){
             i++;
         }
     });
+let j = 0;
+    allCurrentHr.forEach((el)=>{
+        let allDataEl = data[j].timeframes;
+        if(dateTime == 'daily'){
+            el.innerText = allDataEl.daily.current;
+        }else if (dateTime == 'weekly'){
+            el.innerText = allDataEl.weekly.current;
+        }else if(dateTime == 'monthly'){
+            el.innerText = allDataEl.monthly.current;
+        };
+        
+        if ( j == allPreviousHr.length){
+            j = 0;
+        }else{
+            j++;
+        }
+    })
 };
 
 dataChange('daily');
